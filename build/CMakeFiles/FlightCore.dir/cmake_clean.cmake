5f2b296efc5cfb088f55cf0b22b845bbed43e704
franczakZapasowe <russeldurantmvp@gmail.com>
2026-09-18 01:29:43 +0200

    first commit

---
+++ build/CMakeFiles/FlightCore.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlightCore.dir/link.d"
+  "CMakeFiles/FlightCore.dir/main.cpp.o"
+  "CMakeFiles/FlightCore.dir/main.cpp.o.d"
+  "FlightCore"
+  "FlightCore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlightCore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
